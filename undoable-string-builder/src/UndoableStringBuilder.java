70f99db8cc39e5dcbb7da9faae37c6944955aa3d
Omer Priel <omer.priel2@gmail.com>
2022-11-02 12:37:41 +0200

    add undoable-string-builder project

---
+++ undoable-string-builder/src/UndoableStringBuilder.java
@@ -0,0 +1,35 @@
+public class UndoableStringBuilder {
+    public UndoableStringBuilder() {
+
+    }
+
+    public UndoableStringBuilder undo() {
+        return this;
+    }
+
+    public UndoableStringBuilder append(String str) {
+        return this;
+    }
+
+    public UndoableStringBuilder delete(int start, int end) {
+        return this;
+    }
+
+    public UndoableStringBuilder insert(int offset, String str) {
+        return this;
+    }
+
+    public UndoableStringBuilder replace(int start,int end, String str) {
+        return this;
+    }
+
+    public UndoableStringBuilder reverse() {
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        return "TODO";
+    }
+}
