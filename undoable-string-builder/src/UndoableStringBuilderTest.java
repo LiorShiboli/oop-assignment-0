8d7f322bc570d0c189c0b9c8a01600c57de30d9c
Omer Priel <omer.priel2@gmail.com>
2022-11-07 08:42:20 +0200

    add tests

--- undoable-string-builder/src/UndoableStringBuilderTest.java
+++ undoable-string-builder/src/UndoableStringBuilderTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.*;
 
@@ -30,25 +31,121 @@ public class UndoableStringBuilderTest {
         builder.append("B");
         builder.undo();
         assertEquals(builder.toString(), "The first line\nA");
+
+        assertThrows(NullPointerException.class, ()-> {
+            builder.append(null);
+        });
     }
 
     @Test
     public void delete() {
+        UndoableStringBuilder builder = new UndoableStringBuilder("0123456789");
 
+        builder.delete(0, 5);
+        assertEquals(builder.toString(), "6789");
+
+        builder.delete(1, 2);
+        assertEquals(builder.toString(), "78");
+
+        builder.undo();
+        assertEquals(builder.toString(), "6789");
+
+        builder.undo();
+        assertEquals(builder.toString(), "0123456789");
+
+        assertThrows(IndexOutOfBoundsException.class, ()-> {
+            builder.delete(-1, 2);
+        });
+
+        assertThrows(IndexOutOfBoundsException.class, ()-> {
+            builder.delete(0, 20);
+        });
     }
 
     @Test
     public void insert() {
+        UndoableStringBuilder builder = new UndoableStringBuilder();
 
+        builder.insert(0, "0123");
+        assertEquals(builder.toString(), "0123");
+
+        builder.insert(4, "4567");
+        assertEquals(builder.toString(), "01234567");
+
+        builder.undo();
+        assertEquals(builder.toString(), "0123");
+
+        builder.insert(2, "---");
+        assertEquals(builder.toString(), "01---23");
+
+        builder.undo();
+        builder.undo();
+        assertEquals(builder.toString(), "");
+
+        assertThrows(IndexOutOfBoundsException.class, ()-> {
+            builder.insert(-1, "");
+        });
+
+        assertThrows(IndexOutOfBoundsException.class, ()-> {
+            builder.insert(1, "123");
+        });
+
+        assertThrows(NullPointerException.class, ()-> {
+            builder.insert(0, null);
+        });
     }
 
     @Test
     public void replace() {
+        UndoableStringBuilder builder = new UndoableStringBuilder("0123456789");
 
+        builder.replace(0, 4, "----");
+        assertEquals(builder.toString(), "----56789");
+
+        builder.undo();
+        assertEquals(builder.toString(), "0123456789");
+
+        builder.replace(1, 5, "--");
+        assertEquals(builder.toString(), "0--6789");
+
+        builder.replace(1, 2, "AA");
+        assertEquals(builder.toString(), "0AA6789");
+
+        builder.undo();
+        builder.undo();
+        assertEquals(builder.toString(), "0123456789");
+
+        assertThrows(IndexOutOfBoundsException.class, ()-> {
+            builder.delete(-1, 1);
+        });
+
+        assertThrows(IndexOutOfBoundsException.class, ()-> {
+            builder.delete(0, 1);
+        });
     }
 
     @Test
     public void reverse() {
+        UndoableStringBuilder builder = new UndoableStringBuilder("0123456789");
 
+        builder.reverse();
+        assertEquals(builder.toString(), "9876543210");
+
+        builder.reverse();
+        assertEquals(builder.toString(), "0123456789");
+
+        builder.reverse();
+        builder.reverse();
+        assertEquals(builder.toString(), "0123456789");
+
+        builder.undo();
+        assertEquals(builder.toString(), "9876543210");
+
+        builder.undo();
+        assertEquals(builder.toString(), "0123456789");
+
+        builder.undo();
+        builder.undo();
+        assertEquals(builder.toString(), "0123456789");
     }
 }
