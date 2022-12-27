cb03cae6347d45dcc4ff1a9fa7cc6e9e4fe5e3c9
Omer Priel <omer.priel2@gmail.com>
2022-12-27 18:34:34 +0200

    add ConcreteMember

--- src/main/java/observer/ConcreteMember.java
+++ src/main/java/observer/ConcreteMember.java
@@ -1,4 +1,37 @@
 package observer;
 
-public class ConcreteMember {
+public class ConcreteMember implements Member {
+    private String name;
+
+    private int minLength;
+    private String addToEnd;
+
+    public ConcreteMember(String name) {
+        this.name = name;
+        this.minLength = 0;
+        this.addToEnd = null;
+    }
+
+    public ConcreteMember(String name, int minLength, String addToEnd) {
+        this.name = name;
+        this.minLength = Math.max(minLength, 0);
+
+        if (this.minLength == 0) {
+            this.addToEnd = null;
+        } else {
+            this.addToEnd = (addToEnd == null || addToEnd.length() == 0) ? "null" : addToEnd;
+        }
+    }
+
+
+    @Override
+    public void update(UndoableStringBuilder usb) {
+        System.out.println("INFO: Member " + name + " updated.");
+
+        int needed = usb.toString().length() - minLength;
+        while (needed > 0) {
+            usb.append(this.addToEnd);
+            needed -= this.addToEnd.length();
+        }
+    }
 }
